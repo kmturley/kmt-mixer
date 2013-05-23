52511bd0f0ebfc5f5bdb9a8441da4870184b121b
Kim T <kimturley@gmail.com>
2013-05-23 21:17:35 +0100

    classify the code

--- js/Mixer.js
+++ js/Mixer.js
@@ -4,8 +4,7 @@
 */
 
 (function () {
-    'use strict';
-
+    
     function Mixer(id, options) {
         this.el = document.getElementById(id);
         this.options = options;
@@ -13,28 +12,85 @@
     }
     
     Mixer.prototype = {
+        loaded: false,
+        urlList: [],
+        bufferList: [],
+        sources: [],
         init: function () {
+            var me = this;
             console.log('Mixer', this);
-            this.track1 = this.create();
-            this.track2 = this.create();
-            this.load(this.track1, this.options.instrumental);
-            this.load(this.track2, this.options.acapella);
+            
+            if ('webkitAudioContext' in window) {
+                this.context = new webkitAudioContext();
+            }
+            
+            var BUFFERS_TO_LOAD = {
+                kick: 'audio/instrumental.mp3',
+                snare: 'audio/acapella.mp3',
+                hihat: 'audio/hihat.wav'
+            };
+            this.BUFFERS = {};
+            var names = [];
+            var paths = [];
+            for (var name in BUFFERS_TO_LOAD) {
+                var path = BUFFERS_TO_LOAD[name];
+                names.push(name);
+                paths.push(path);
+            }
+            var bufferLoader = new BufferLoader(me.context, paths, function(bufferList) {
+                for (var i = 0; i < bufferList.length; i++) {
+                    var buffer = bufferList[i];
+                    var name = names[i];
+                    me.BUFFERS[name] = buffer;
+                    console.log('BufferLoader', name);
+                }
+                me.loaded = true;
+            });
+            bufferLoader.load();
         },
-        create: function () {
-            var track = document.createElement('audio');
-            track.setAttribute('loop', true);
-            track.addEventListener('canplaythrough', function () {
-                track.play();
-                console.log(track.duration);
-                console.log(track.src);
-            }, true);
-            return track;
+        play: function() {
+            if (this.loaded === true) {
+                console.log('play');
+                var me = this;
+                this.sources = [];
+                function playSound(buffer, time) {
+                    var source = me.context.createBufferSource();
+                    source.buffer = buffer;
+                    source.connect(me.context.destination);
+                    if (!source.start) source.start = source.noteOn;
+                    source.start(time);
+                    me.sources.push(source);
+                }
+                var kick = this.BUFFERS.kick;
+                var snare = this.BUFFERS.snare;
+                var hihat = this.BUFFERS.hihat;
+                var startTime = this.context.currentTime + 0.100;
+                var tempo = 90;
+                var eighthNoteTime = (120 / tempo);
+                
+                playSound(snare, startTime + 0 * 8 * eighthNoteTime);
+                
+                for (var bar = 0; bar < 8; bar++) {
+                    var time = startTime + bar * 8 * eighthNoteTime;
+                    playSound(kick, time);
+                    
+                    //playSound(snare, time + 2 * eighthNoteTime);
+                    //playSound(snare, time + 6 * eighthNoteTime);
+                    for (var i = 0; i < 8; ++i) {
+                      playSound(hihat, time + i * eighthNoteTime);
+                    }
+                }
+            }
         },
-        load: function (track, url) {
-            console.log('mp3', track.canPlayType('audio/mpeg'));
-            track.setAttribute('src', url);
-            track.load();
-            console.log('load', url);
+        stop: function() {
+            if (this.loaded === true) {
+                console.log('stop');
+                for (var i=0; this.sources.length; i++) {
+                    if (this.sources[i]) {
+                        this.sources[i].stop(0);
+                    }
+                }
+            }
         }
     };
     
