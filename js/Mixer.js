f084d426ebc0e80bf4ed0bcfb2004d4abace577f
Kim T <kimturley@gmail.com>
2013-05-23 23:01:24 +0100

    tidy 1

--- js/Mixer.js
+++ js/Mixer.js
@@ -16,27 +16,32 @@
         urlList: [],
         bufferList: [],
         sources: [],
+        BUFFERS: {},
         init: function () {
-            var me = this;
+            var me = this,
+                names = [],
+                paths = [];
+            var BUFFERS_TO_LOAD = {
+                    kick: 'audio/instrumental.mp3',
+                    kick2: 'audio/improve.mp3',
+                    snare: 'audio/acapella.mp3'
+                    //hihat: 'audio/hihat.wav'
+                };
             console.log('Mixer', this);
             
-            if ('webkitAudioContext' in window) {
-                this.context = new webkitAudioContext();
+            try {
+                window.AudioContext = window.AudioContext || window.webkitAudioContext;
+                this.context = new AudioContext();
+            } catch (e) {
+                alert("Web Audio API is not supported in this browser");
             }
-            
-            var BUFFERS_TO_LOAD = {
-                kick: 'audio/instrumental.mp3',
-                snare: 'audio/acapella.mp3',
-                hihat: 'audio/hihat.wav'
-            };
-            this.BUFFERS = {};
-            var names = [];
-            var paths = [];
             for (var name in BUFFERS_TO_LOAD) {
                 var path = BUFFERS_TO_LOAD[name];
                 names.push(name);
                 paths.push(path);
             }
+            
+            console.log(names, paths);
             var bufferLoader = new BufferLoader(me.context, paths, function(bufferList) {
                 for (var i = 0; i < bufferList.length; i++) {
                     var buffer = bufferList[i];
@@ -62,23 +67,26 @@
                     me.sources.push(source);
                 }
                 var kick = this.BUFFERS.kick;
+                var kick2 = this.BUFFERS.kick2;
                 var snare = this.BUFFERS.snare;
                 var hihat = this.BUFFERS.hihat;
                 var startTime = this.context.currentTime + 0.100;
-                var tempo = 90;
+                var tempo = Math.round(60/(kick.duration/16));
                 var eighthNoteTime = (120 / tempo);
-                
+
                 playSound(snare, startTime + 0 * 8 * eighthNoteTime);
                 
                 for (var bar = 0; bar < 8; bar++) {
-                    var time = startTime + bar * 8 * eighthNoteTime;
-                    playSound(kick, time);
-                    
-                    //playSound(snare, time + 2 * eighthNoteTime);
-                    //playSound(snare, time + 6 * eighthNoteTime);
-                    for (var i = 0; i < 8; ++i) {
-                      playSound(hihat, time + i * eighthNoteTime);
+                    var time = startTime + bar * (8 * eighthNoteTime);
+                    if (bar % 2 === 0) {
+                        playSound(kick, time);
+                    } else {
+                        playSound(kick2, time);
                     }
+                    /*
+                    for (var i = 0; i < 8; ++i) {
+                      playSound(hihat, time + (i * eighthNoteTime));
+                    }*/
                 }
             }
         },
