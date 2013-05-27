e0bd27bc504529e3fd17b8fe9ac53b5843bbe4f1
Kim T <kimturley@gmail.com>
2013-05-27 13:57:43 +0100

    tidy up
    coding standards!

--- js/Mixer.js
+++ js/Mixer.js
@@ -1,9 +1,10 @@
-/*globals console, window, document*/
+/*globals console, window, document, alert, BufferLoader*/
 /*
 *   Mixer
 */
 
 (function () {
+    'use strict';
     
     function Mixer(id, options) {
         this.el = document.getElementById(id);
@@ -13,93 +14,55 @@
     
     Mixer.prototype = {
         loaded: false,
-        urlList: [],
-        bufferList: [],
-        sources: [],
-        BUFFERS: {},
+        audio: [],
         init: function () {
+            try { this.context = window.AudioContext ? new window.AudioContext() : new window.webkitAudioContext(); } catch (e) { alert('Web Audio API not supported'); }
+            this.buffer(this.options.files);
+        },
+        buffer: function (files) {
+            console.log('Mixer.buffer', files);
             var me = this,
-                names = [],
-                paths = [];
-            var BUFFERS_TO_LOAD = {
-                    kick: 'audio/instrumental.mp3',
-                    kick2: 'audio/improve.mp3',
-                    snare: 'audio/acapella.mp3'
-                    //hihat: 'audio/hihat.wav'
-                };
-            console.log('Mixer', this);
-            
-            try {
-                window.AudioContext = window.AudioContext || window.webkitAudioContext;
-                this.context = new AudioContext();
-            } catch (e) {
-                alert("Web Audio API is not supported in this browser");
-            }
-            for (var name in BUFFERS_TO_LOAD) {
-                var path = BUFFERS_TO_LOAD[name];
-                names.push(name);
-                paths.push(path);
-            }
-            
-            console.log(names, paths);
-            var bufferLoader = new BufferLoader(me.context, paths, function(bufferList) {
-                for (var i = 0; i < bufferList.length; i++) {
-                    var buffer = bufferList[i];
-                    var name = names[i];
-                    me.BUFFERS[name] = buffer;
-                    console.log('BufferLoader', name);
-                }
-                me.loaded = true;
-            });
+                i = 0,
+                bufferLoader = new BufferLoader(me.context, files, function (items) {
+                    for (i = 0; i < items.length; i += 1) {
+                        me.audio.push(items[i]);
+                    }
+                    me.loaded = true;
+                    console.log('Mixer.buffer.complete', me.audio);
+                });
             bufferLoader.load();
         },
-        play: function() {
+        play: function () {
             if (this.loaded === true) {
-                console.log('play');
-                var me = this;
-                this.sources = [];
-                function playSound(buffer, time) {
-                    var source = me.context.createBufferSource();
-                    source.buffer = buffer;
-                    source.connect(me.context.destination);
-                    if (!source.start) source.start = source.noteOn;
-                    source.start(time);
-                    me.sources.push(source);
-                }
-                var kick = this.BUFFERS.kick;
-                var kick2 = this.BUFFERS.kick2;
-                var snare = this.BUFFERS.snare;
-                var hihat = this.BUFFERS.hihat;
-                var startTime = this.context.currentTime + 0.100;
-                var tempo = Math.round(60/(kick.duration/16));
-                var eighthNoteTime = (120 / tempo);
-
-                playSound(snare, startTime + 0 * 8 * eighthNoteTime);
+                var me = this,
+                    startTime = this.context.currentTime + 0.100,
+                    tempo = Math.round(60 / (this.audio[0].duration / 16)),
+                    eighthNoteTime = (120 / tempo),
+                    bar = 0;
                 
-                for (var bar = 0; bar < 8; bar++) {
-                    var time = startTime + bar * (8 * eighthNoteTime);
+                // play the acapella
+                this.load(this.audio[2], startTime + bar * (8 * eighthNoteTime));
+                
+                for (bar = 0; bar < 8; bar += 1) {
+                    // every bar switch between instrumentals
                     if (bar % 2 === 0) {
-                        playSound(kick, time);
+                        this.load(this.audio[0], startTime + bar * (8 * eighthNoteTime));
                     } else {
-                        playSound(kick2, time);
+                        this.load(this.audio[1], startTime + bar * (8 * eighthNoteTime));
                     }
-                    /*
-                    for (var i = 0; i < 8; ++i) {
-                      playSound(hihat, time + (i * eighthNoteTime));
-                    }*/
                 }
             }
         },
-        stop: function() {
-            if (this.loaded === true) {
-                console.log('stop');
-                for (var i=0; this.sources.length; i++) {
-                    if (this.sources[i]) {
-                        this.sources[i].stop(0);
-                    }
-                }
+        load: function (buffer, time) {
+            var source = this.context.createBufferSource();
+            source.buffer = buffer;
+            source.connect(this.context.destination);
+            if (!source.start) {
+                source.start = source.noteOn;
             }
-        }
+            source.start(time);
+        },
+        stop: function () {}
     };
     
     window.Mixer = Mixer;
